aff9c8d94f41d8c9ab583383e9a098b686265d91
Nik0loz <nikolozmelikidze85@gmail.com>
2025-12-01 22:54:07 +0400

    shesrulebuli davaleba

---
+++ davaleba.js
@@ -0,0 +1,56 @@
+
+
+/*for (let i = 2; i <= 8; i++) {
+    console.log(i);
+}*/
+
+
+/*for (let i = 5; i <=35; i += 4) {
+  console.log(i);
+}*/
+
+
+/*let a = 1;
+
+for (let i = 3; i <= 8; i++) {
+  a = a * i;
+}
+console.log(a);*/
+
+
+/*const person = {
+firstname: "nika",
+lastname: "melikidze", 
+age : 40
+}
+console.log(person.firstname + " " + person.lastname);*/
+
+
+
+/*const person = {
+firstname: "nika",
+lastname: "melikidze", 
+age : 40
+}
+console.log(person.firstname);
+   console.log (person.lastname);
+      console.log (person.age); */
+
+
+/*let fruit = ["Apple", "Banana", "Orange"];
+
+for (let i = 0; i<fruit.length; i++)
+{
+  console.log(fruit[i]);
+}*/
+
+
+/*let fruit = ["Apple", "Banana", "Orange"];
+fruit.unshift("grapes");
+fruit.push("Pineapples");
+console.log(fruit);*/
+
+/*let fruits = ["Apple", "Banana", "Orange", "Mango", "Pineapples", "Grapes"];
+for (let i = 1; i < fruits.length; i +=2) {
+  console.log("Index:", i, "Value:", fruits[i]);
+}*/
